561e5563ce3ceea3d74803c9e0a7ad900921e59d
carlosdh86 <47611812+carlosdh86@users.noreply.github.com>
2019-05-04 22:18:54 +0200

    commit 2

--- src/com/ciceonline/Main.java
+++ src/com/ciceonline/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) {
 
         Thread cronometro = new Thread(() -> {
-            int segundos = 0, minutos = 0, horas = 1;
+            int segundos = 0, minutos = 0, horas = 0;
             while (true) {
                 try {
                     Thread.sleep(1000);
